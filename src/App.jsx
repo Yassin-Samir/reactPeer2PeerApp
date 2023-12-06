5970a88a567dcce8f2a04b35592701c8c0013689
yassin samir <yassinsam3@gmail.com>
2023-12-07 00:22:23 +0200

    refactor: Update App component to use useReducer and refactor connection creation logic
    This commit refactors the `App` component in `src/App.jsx` to utilize
    the `useReducer` hook instead of multiple `useState` hooks. It also
    refactors the logic for creating a new connection and joining a call.
    
    The `App` component now uses `useReducer` to manage the state, with the
    `reducer` function handling actions related to creating a call or
    joining an existing call. The connection creation logic has been
    abstracted into a separate `CreateNewConnection` function, which either
    creates a new `RTCPeerConnection` object or uses an existing one.
    
    These changes improve code organization and maintainability by reducing
    duplication and separating concerns. They also ensure that the
    connection creation logic is consistent across different scenarios,
    improving the reliability of the call functionality.
    
    No issues are addressed in this commit.
    
    (refs #123)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useReducer, useRef, useState } from "react";
 import {
   collection,
   getDoc,
@@ -16,108 +16,179 @@ const RtcServers = {
   ],
 };
 const CallDocsCollection = collection(db, "calldocs");
+async function GetVideoAndAudioStream() {
+  const rtcLocalMediaStream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  });
+  const LocalVideoOnlyStream = new MediaStream();
+  rtcLocalMediaStream
+    .getVideoTracks()
+    .forEach((track) => LocalVideoOnlyStream.addTrack(track));
+  return { LocalVideoOnlyStream, rtcLocalMediaStream };
+}
+function CreateNewConnection(rtcConnection, RemoteStream) {
+  console.log(rtcConnection?.currentRemoteDescription);
+  const connection =
+    !rtcConnection?.currentRemoteDescription &&
+    typeof rtcConnection === "object"
+      ? rtcConnection
+      : new RTCPeerConnection(RtcServers);
+  connection.ontrack &&
+    (connection.ontrack = (event) => {
+      event.streams[0]
+        .getTracks()
+        .forEach((track) => RemoteStream.addTrack(track));
+    });
+  return connection;
+}
+function reducer(state, { action: { JoinCallId, RemoteStream }, type }) {
+  const { CallDocId, RTCConnection, callDoc } = state;
+  const connection = CreateNewConnection(RTCConnection, RemoteStream);
+  switch (type) {
+    case "createCall":
+      const callDoc = doc(CallDocsCollection);
+      createCall(callDoc, connection);
+      return {
+        callDoc,
+        RTCConnection: connection,
+        CallDocId: callDoc.id,
+      };
+    case "JoinCall":
+      JoinCall(JoinCallId, connection);
+      return state;
+    default:
+      break;
+  }
+  async function JoinCall(JoinCallId, connection) {
+    const docRef = doc(CallDocsCollection, JoinCallId);
+    const data = (await getDoc(docRef)).data();
+    const { type, sdp } = data.offer;
+    const RemoteDescription = new RTCSessionDescription({
+      type,
+      sdp,
+    });
+    await connection.setRemoteDescription(RemoteDescription);
+    const answer = await connection.createAnswer();
+    await connection.setLocalDescription(answer);
+    console.log({ connection });
+    console.log({ RemoteDescription, answer });
+    connection.onicecandidate = (event) => {
+      event.candidate &&
+        setDoc(docRef, {
+          ...data,
+          answer: {
+            sdp: connection.localDescription.sdp,
+            type: answer.type,
+          },
+        });
+    };
+  }
+  async function createCall(callDoc, connection) {
+    if (!connection) return;
+    const offer = await connection.createOffer();
+    await setDoc(callDoc, {
+      offer: { sdp: offer.sdp, type: offer.type },
+    });
+    await connection.setLocalDescription(offer);
+    connection.onicecandidate = async (event) => {
+      event.candidate &&
+        (await setDoc(callDoc, {
+          offer: {
+            sdp: connection.localDescription.sdp,
+            type: offer.type,
+          },
+        }));
+    };
+  }
+}
 function App() {
-  const [CallDocId, setCallDocId] = useState("");
   const [JoinCallId, setJoinCallId] = useState("");
-  const RemoteStream = useMemo(() => new MediaStream(), []);
-  const RTCConnection = useMemo(() => new RTCPeerConnection(RtcServers), []);
   const LocalVideo = useRef();
   const RemoteVideo = useRef();
+
+  const [state, dispatch] = useReducer(
+    reducer,
+    {
+      CallDocId: "",
+      RTCConnection: new RTCPeerConnection(RtcServers),
+      callDoc: null,
+    },
+    (state) => {
+      state.RTCConnection.ontrack = (event) => {
+        event.streams[0]
+          .getTracks()
+          .forEach((track) => RemoteStream.addTrack(track));
+      };
+      return state;
+    }
+  );
+  const { CallDocId, RTCConnection, callDoc } = state;
+  const RemoteStream = useMemo(() => new MediaStream(), [RTCConnection]);
+  const UnListen = useMemo(() => {
+    if (!callDoc) return () => {};
+    return onSnapshot(callDoc, async (Snapshot) => {
+      console.log("snappy");
+      try {
+        const data = Snapshot.data();
+        if (!data?.answer) return;
+        const { type, sdp } = data.answer;
+        const RemoteDescription = new RTCSessionDescription({
+          type,
+          sdp,
+        });
+        if (!RTCConnection.remoteDescription)
+          await RTCConnection.setRemoteDescription(RemoteDescription);
+      } catch (error) {
+        console.log({ snapshotError: error });
+      }
+    });
+  }, [callDoc]);
   useEffect(() => {
     (async () => {
       try {
-        const rtcLocalMediaStream = await navigator.mediaDevices.getUserMedia({
-          audio: true,
-          video: true,
-        });
-        const videoStream = new MediaStream();
-        rtcLocalMediaStream
-          .getVideoTracks()
-          .forEach((track) => videoStream.addTrack(track));
-        LocalVideo.current.srcObject = videoStream;
         RemoteVideo.current.srcObject = RemoteStream;
+        if (LocalVideo.current.srcObject) {
+          console.log({ src: RemoteVideo.current.srcObject });
+          return;
+        }
+        const { LocalVideoOnlyStream, rtcLocalMediaStream } =
+          await GetVideoAndAudioStream();
+        LocalVideo.current.srcObject = LocalVideoOnlyStream;
         rtcLocalMediaStream
           .getTracks()
           .forEach((track) =>
             RTCConnection.addTrack(track, rtcLocalMediaStream)
           );
-        RTCConnection.ontrack = (event) => {
-          event.streams[0]
-            .getTracks()
-            .forEach((track) => RemoteStream.addTrack(track));
-        };
       } catch (error) {
         console.log({ error });
       }
     })();
-  }, []);
-  const createCall = async () => {
-    try {
-      const callDoc = doc(CallDocsCollection);
-      onSnapshot(callDoc, async (Snapshot) => {
-        try {
-          const data = Snapshot.data();
-          if (!data.answer) return;
-          const { type, sdp } = data.answer;
-          const RemoteDescription = new RTCSessionDescription({
-            type,
-            sdp,
-          });
-          console.log({ type, sdp });
-          if (!RTCConnection.remoteDescription)
-            await RTCConnection.setRemoteDescription(RemoteDescription);
-        } catch (error) {
-          console.log({ snapShotError: error });
-        }
-      });
-      const offer = await RTCConnection.createOffer();
-      await setDoc(callDoc, {
-        offer: { sdp: offer.sdp, type: offer.type },
-      });
-      await RTCConnection.setLocalDescription(offer);
-      RTCConnection.onicecandidate = async (event) => {
-        event.candidate &&
-          (await setDoc(callDoc, {
-            offer: {
-              sdp: RTCConnection.localDescription.sdp,
-              type: offer.type,
-            },
-          }));
-      };
-      setCallDocId(callDoc.id);
-    } catch (error) {
-      console.log({ error });
-    }
+    return () => {
+      UnListen();
+      RemoteVideo.current.srcObject = null;
+    };
+  }, [RTCConnection]);
+  const createCall = () => {
+    dispatch({
+      type: "createCall",
+      action: {
+        RemoteStream,
+      },
+    });
   };
-  const JoinCall = async () => {
+  const JoinCall = () => {
     if (!JoinCallId) {
       alert("Please Enter A Call Id");
       return;
     }
-    try {
-      const docRef = doc(CallDocsCollection, JoinCallId);
-      const data = (await getDoc(docRef)).data();
-      const { type, sdp } = data.offer;
-      const RemoteDescription = new RTCSessionDescription({
-        type,
-        sdp,
-      });
-      await RTCConnection.setRemoteDescription(RemoteDescription);
-      const answer = await RTCConnection.createAnswer();
-      await RTCConnection.setLocalDescription(answer);
-      RTCConnection.onicecandidate = (event) => {
-        event.candidate &&
-          setDoc(docRef, {
-            ...data,
-            answer: {
-              sdp: RTCConnection.localDescription.sdp,
-              type: answer.type,
-            },
-          });
-      };
-    } catch (error) {
-      console.log({ error });
-    }
+    dispatch({
+      type: "JoinCall",
+      action: {
+        JoinCallId,
+        RemoteStream,
+      },
+    });
   };
 
   return (
@@ -127,7 +198,7 @@ function App() {
         <video ref={RemoteVideo} autoPlay playsInline></video>
       </div>
       {CallDocId && <p>{CallDocId}</p>}
-      <button onClick={createCall}>create Call</button>
+      <button onClick={createCall}>create a new Call</button>
       <input
         type="text"
         value={JoinCallId}
